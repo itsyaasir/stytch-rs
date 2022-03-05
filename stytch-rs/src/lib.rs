669624f1b4133a9804c65034d4ced4124da68e81
Yasir Shariff <goodwonder5@gmail.com>
2022-03-05 21:02:34 +0300

    removed dummy code

--- stytch-rs/src/lib.rs
+++ stytch-rs/src/lib.rs
@@ -1,12 +1,3 @@
-pub mod model;
 pub mod api;
 pub mod errors;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub mod model;
